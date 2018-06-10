e521f0e6c9dbe8b4ee7a63ceb059f30084cea2b5
bhagwat <bhagwat@gmail.com>
2018-06-10 21:57:50 +0530

    wish new class

--- Student/src/com/nt/test/Test.java
+++ Student/src/com/nt/test/Test.java
@@ -1,9 +1,13 @@
 package com.nt.test;
 
+import com.nt.wish.WishGenerator;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Student Info");
+		WishGenerator wish=new WishGenerator();
+		System.out.println(wish.generateWish("bhagwat"));
 
 	}
 
---
+++ Student/src/com/nt/wish/WishGenerator.java
@@ -0,0 +1,9 @@
+package com.nt.wish;
+
+public class WishGenerator {
+
+	public String generateWish(String user)
+	{
+		return "Good Morning"+user;
+	}
+}
