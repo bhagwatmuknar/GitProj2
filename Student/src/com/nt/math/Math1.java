47afbf33fa6757ee610749e61edf85128dfd22f0
bhagwat <bhagwat@gmail.com>
2018-06-10 22:21:30 +0530

    new math class impl

---
+++ Student/src/com/nt/math/Math1.java
@@ -0,0 +1,10 @@
+package com.nt.math;
+
+public class Math1 {
+	public int sum(int x, int y)
+	{
+		int total=x+y;
+		return total;
+	}
+
+}
--- Student/src/com/nt/test/Test.java
+++ Student/src/com/nt/test/Test.java
@@ -1,14 +1,17 @@
 package com.nt.test;
 
+import com.nt.math.Math1;
 import com.nt.wish.WishGenerator;
-
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Student Info");
 		WishGenerator wish=new WishGenerator();
-		System.out.println(wish.generateWish("bhagwat"));
-
+		System.out.println(wish.generateWish("   bhagwat"));
+		Math1 math=new Math1();
+		System.out.println(math.sum(10,20));
+		
+		
 	}
 
 }
